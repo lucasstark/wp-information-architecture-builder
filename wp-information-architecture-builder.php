35c6353ef457d3d1607bd6765fd0d00eac4afbfe
Lucas Stark <lucas.stark@live.com>
2017-01-21 09:12:28 -0500

    Localize schema from root / default site.

--- wp-information-architecture-builder.php
+++ wp-information-architecture-builder.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 Plugin Name:  WordPress Information Architecture Builder ( BETA )
 Plugin URI: https://github.com/lucasstark/wp-information-architecture-builder
@@ -136,6 +137,48 @@ class WP_IAB_Main {
 		);
 
 		wp_localize_script( 'wpiab-main', 'wp_iab_params', $params );
+		$this->json_api_client_js();
+	}
+
+	private function json_api_client_js() {
+		/**
+		 * @var WP_REST_Server $wp_rest_server
+		 */
+		global $wp_rest_server;
+		// Ensure the rest server is intiialized.
+		if ( empty( $wp_rest_server ) ) {
+			/** This filter is documented in wp-includes/rest-api.php */
+			$wp_rest_server_class = apply_filters( 'wp_rest_server_class', 'WP_REST_Server' );
+			$wp_rest_server       = new $wp_rest_server_class();
+			/** This filter is documented in wp-includes/rest-api.php */
+			do_action( 'rest_api_init', $wp_rest_server );
+		}
+		// Load the schema.
+		$schema_request  = new WP_REST_Request( 'GET', '/wp/v2' );
+		$schema_response = $wp_rest_server->dispatch( $schema_request );
+		$schema          = null;
+		if ( ! $schema_response->is_error() ) {
+			$schema = $schema_response->get_data();
+		}
+
+		// Localize the plugin settings and schema.
+		$settings = array(
+			'root'          => esc_url_raw( get_rest_url() ),
+			'nonce'         => wp_create_nonce( 'wp_rest' ),
+			'versionString' => 'wp/v2/',
+			'schema'        => $schema,
+			'cacheSchema'   => true,
+		);
+		/**
+		 * Filter the JavaScript Client settings before localizing.
+		 *
+		 * Enables modifying the config values sent to the JS client.
+		 *
+		 * @param array $settings The JS Client settings.
+		 */
+
+		$settings = apply_filters( 'rest_js_client_settings', $settings );
+		wp_localize_script( 'wp-api', 'wpApiSettings', $settings );
 	}
 
 
